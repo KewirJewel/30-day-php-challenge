07a08bdec39e9cc18e3d784edc4bac49536b14ca
KewirJewel <kewirlinx@gmail.com>
2024-08-03 09:57:28 -0700

    Printing Hello World

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+ <style>
+ 
+    body {
+                   font-family: Arial, sans-serif;
+                   text-align: center;
+                   background-color: gray;
+                   color: white;
+                   font-size: 30%;
+                   text-size-adjust: 30%;
+               }
+    text {
+                text-decoration-color: white;
+                
+    }</style>
+
+</head>
+<body>
+   <?php 
+   $world = "Helloworld";
+   echo $world
+   ?>
+
+</body>
+</html> 
+ 
+ 
